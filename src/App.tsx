4f73851d24c9aa430666f1b843c7d7b8c1c94652
ViacheslavKolosInox <viacheslav.kolos@inoxoft.com>
2023-07-23 13:56:03 +0300

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react'
 
 const App: FC = () => {
-  return <div>{/* Your solution here*/}</div>
+  return <h1 className="text-3xl font-bold underline">
+    Hello world!
+    <h2></h2>
+  </h1>
 }
 
 export default App
